5e0cb00b415e1eeb7559bd723e3447ee1356ded3
Dirrk <Derekrada@gmail.com>
2014-12-26 18:42:34 -0500

    refactored the 3 route handlers into one

--- routes/group.js
+++ routes/group.js
@@ -10,56 +10,12 @@ var userAuth = require('../middleware/userAuthentication');
 
 // match the id first
 router.use(userAuth);
-router.get('/:id', groupHandler); // /group/id
+router.get('/:id', httpHelper.searchRouteHandler); // /group/id
 router.get('/', groupsHandler); // /groups
 
 function groupsHandler(req, res) {
 
     res.json({ success: true, locations: req.session.groups || []});
-
-};
-
-function groupHandler(req, res) {
-
-    var location = req.params.id;
-    var startDate = parseInt(req.query.start) || 0;
-    var endDate = parseInt(req.query.end) || 4389369600000;
-    var userId = req.session.userId;
-    var page = parseInt(req.query.page) || 1;
-
-    var options = {
-
-        userId: userId,
-        locationId: location,
-        startDate: startDate,
-        endDate: endDate,
-        page: page
-    };
-
-    httpHelper.locationHistory(options, function (results) {
-
-        var ret = JSON.parse(JSON.stringify(options));
-        if (results && results.length) {
-
-            var pageStart = (page - 1) * 50;
-            var pageEnd = (page) * 50;
-            if (pageEnd > results.length) {
-                pageEnd = results.length;
-            }
-            if (pageStart > results.length) {
-                pageStart = 0;
-            }
-            ret.success = true;
-            ret.total = results.length;
-            ret.data = results.slice(pageStart, pageEnd);
-            res.json(ret);
-
-        } else {
-            ret.success = false;
-            ret.data = [];
-            res.json(ret);
-        }
-    });
 
 };
 
--- routes/search.js
+++ routes/search.js
@@ -9,53 +9,8 @@ var userAuth = require('../middleware/userAuthentication');
 
 
 router.use(userAuth);
-router.post('/:locationId', searchHandler); // /search/location
+router.post('/:locationId', httpHelper.searchRouteHandler); // /search/location
 
-
-function searchHandler(req, res, next) {
-
-    var location = req.params.locationId;
-    var startDate = parseInt(req.query.start) || 0;
-    var endDate = parseInt(req.query.end) || 4389369600000;
-    var query = req.body.query;
-    var userId = req.session.userId;
-    var page = parseInt(req.query.page) || 1;
-
-    var options = {
-
-        userId: userId,
-        locationId: location,
-        startDate: startDate,
-        endDate: endDate,
-        query: query,
-        page: page
-    };
-
-    httpHelper.locationHistory(options, function (results) {
-
-        var ret = JSON.parse(JSON.stringify(options));
-        if (results && results.length) {
-
-            var pageStart = (page - 1) * 50;
-            var pageEnd = (page) * 50;
-            if (pageEnd > results.length) {
-                pageEnd = results.length;
-            }
-            if (pageStart > results.length) {
-                pageStart = 0;
-            }
-            ret.success = true;
-            ret.total = results.length;
-            ret.data = results.slice(pageStart, pageEnd);
-            res.json(ret);
-
-        } else {
-            ret.success = false;
-            ret.data = [];
-            res.json(ret);
-        }
-    });
-}
 
 module.exports = router;
 
